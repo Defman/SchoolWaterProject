e904c96ebaa30ff76a697b66d2ebec487edea45f
Jacob Emil <Jacob@Rosborg.dk>
2016-05-31 12:50:44 +0200

    Changes

--- water.c
+++ water.c
@@ -1,12 +1,11 @@
 #include "simpletools.h"
 #include "fdserial.h"
 #include "adcDCpropab.h"
-#include "math.h"
 
 double min;
 double max;
 int volume;
-double scale = 1;
+double scale;
 
 double max_double(double a, double b) {
   if (a > b) {
@@ -22,7 +21,7 @@ double readInput() {
   while (reads < 10) {
     double read = adc_volts(3);
     if (read > last * 0.95 && read < last * 1.05) {
-      last = (read + last) / 2
+      last = (last + read) / 2;
       reads = reads + 1;
     } else {
       last = read;
@@ -34,8 +33,7 @@ double readInput() {
 }
 
 double readWeight() {
-  double last = readInput();
-  return max_double(last * scale + min, 0);
+  return max_double(readInput() * scale + min, 0);
 }
 
 void writeData() {
@@ -51,24 +49,52 @@ void sendData() {
   printf("%s\n", "I send data from the propeller to the app via bluetooth =)");
 }
 
+void writeConfig() {
+  FILE* fp = fopen("config", "w");
+  fwrite(min, sizeof(min), 1, fp);
+  fwrite(max, sizeof(max), 1, fp);
+  fwrite(volume, sizeof(volume), 1, fp);
+  fwrite(scale, sizeof(scale), 1, fp);
+  fclose(fp);
+}
+
+void readConfig() {
+  FILE* fp = fopen("config", "r");
+  fread(&min, 4, 1, fp);
+  fread(&max, 4, 1, fp);
+  fread(&volume, 4, 1, fp);
+  fread(&scale, 4, 1, fp);
+  fclose(fp);
+}
+
+void writeData(long timestamp, int volume) {
+  FILE* fp = fopen("data", "w");
+  fwrite(timestamp, sizeof(timestamp), 1, fp);
+  fwrite(volume, sizeof(volume), 1, fp);
+  fclose(fp);
+}
+
 void init() {
-  printf("%s\n", "Please enter the containers volume in mL?");
+  printf("Please enter the bottle's volume in mL.\n");
   scan("%d", &volume);
-  printf("%s\n", "Place the empty container on the weight, and press any key.");
+
+  printf("Place the empty bottle on the weight, and press any key.\n");
   scan("");
   min = readInput();
-  printf("Fill the container to %dmL, place it on the weight, and press any key.\n", volume);
+
+  printf("Fill the bottle to %imL, place it on the weight, and press any key.\n", volume);
   scan("");
   max = readInput();
 
   scale = volume/(max-min);
   min = -scale*min;
+  max = volume * 1.1;
 }
 
 int main() {
+  printf("This is automatic water bottle mesurer made by Abdul, Tabita, and Jacob.\n");
   adc_init(21, 20, 19, 18);
   init();
-  printf("%s\n", "I'm the main function =)");
   int id = 0;
   while (1) {
     printf("Reading %i: %f\n", id, readWeight());
